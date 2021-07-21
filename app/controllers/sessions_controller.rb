3729be7b5082a22bc56a00e729b922792ac2a0e0
DanteAlonsoHT <dante1818@hotmail.com>
2021-07-21 14:43:30 -0500

    Add logic for sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_name(params[:name])
+    if user
+      session[:user_id] = user.id
+      redirect_to root_url, notice: "Logged in!"
+    else
+      flash.now[:alert] = "Account is invalid"
+      render "new"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Logged out!"
   end
 end
