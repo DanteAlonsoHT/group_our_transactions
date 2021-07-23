0d4c56fd1bb200c09416158ab022b334739a9842
DanteAlonsoHT <dante1818@hotmail.com>
2021-07-23 17:07:12 -0500

    Add features for unique method in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
+  before_action :authenticated, only: %i[ index ]
+
   def index
+    if Group.all.where(id: 5).first == nil
+      Group.create(id: 5, name: "No group", user_id: 1)
+    end
+  end
+
+  private
+
+  helper_method :logged_in?
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authenticated
+    unless self.logged_in?
+      flash[:alert] = 'You need to login or sign up to access'
+      redirect_to '/login'
+    end
   end
 end
