2d6746d5aac472543a1b2e99e414119e7c31876b
DanteAlonsoHT <dante1818@hotmail.com>
2021-07-23 17:00:12 -0500

    Add link for external transactions

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,5 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
+  get 'investments-ext', to: 'investments#index_external', as: 'external_investments'
 end
