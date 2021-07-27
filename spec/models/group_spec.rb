21a8a50ebd8ce6e6ecfbe7e099052f6aa85d8269
DanteAlonsoHT <dante1818@hotmail.com>
2021-07-27 03:18:29 -0500

    Add more unit test

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -6,8 +6,32 @@ RSpec.describe Group, type: :model do
   let(:group) { Group.create(name: 'Group1', user_id: user.id, id: 1) }
   let(:fatal_group) { Group.new(name: 'Group2') }
 
+  let(:group_by_ghost_user) { Group.create(name: 'Group1', user_id: 100_000) }
+
   let(:investment) { Investment.new(crypto: 'Bitcoin', amount: 0.0055, user_id: user.id) }
   let(:investment2) { Investment.new(crypto: 'Dogecoin', amount: 2030.5, user_id: user.id) }
+
+  let(:too_short_name_group) { Group.create(name: 'a', user_id: user.id, id: 1) }
+  let(:too_long_name_group) do
+    Group.create(name: 'In another life, I would be your girl, we have keep all our
+                                                  promises be us against the world', user_id: user.id, id: 1)
+  end
+
+  describe 'Validating name group' do
+    it 'if the name group is too short, it should not be valid' do
+      expect(too_short_name_group).to_not be_valid
+    end
+
+    it 'if the name group is too loog, it should not be valid as well' do
+      expect(too_long_name_group).to_not be_valid
+    end
+  end
+
+  describe 'User must exist to create a group' do
+    it "Is not valid if user who doesn't exist is trying to create a group" do
+      expect(group_by_ghost_user).to_not be_valid
+    end
+  end
 
   describe 'Group can be created and validated' do
     it 'User can create a group' do
--- spec/models/investment_spec.rb
+++ spec/models/investment_spec.rb
@@ -5,9 +5,37 @@ RSpec.describe Investment, type: :model do
   let(:user) { User.create(name: 'User1') }
   let(:group) { Group.create(name: 'Group1', user_id: user.id) }
   let(:investment) { Investment.create(crypto: 'Bitcoin', amount: 0.0055, user_id: user.id, group_id: group.id) }
+
+  let(:short_investment) do
+    Investment.create(crypto: 'Bitcoin', amount: 0.0000249925, user_id: user.id, group_id: group.id)
+  end
+  let(:long_investment) do
+    Investment.create(crypto: 'Bitcoin', amount: 10_000_000_000, user_id: user.id, group_id: group.id)
+  end
+  let(:negative_investment) do
+    Investment.create(crypto: 'Bitcoin', amount: -0.000001, user_id: user.id, group_id: group.id)
+  end
+
   let(:fatal_investment_amount) { Investment.new(crypto: 'Dogecoin', user_id: user.id) }
   let(:fatal_investment_crypto) { Investment.new(amount: 0.0055, user_id: user.id) }
   let(:fatal_investment_user_id) { Investment.new(crypto: 'Dogecoin', amount: 0.0055, group_id: group.id) }
+
+  let(:investment_to_ghost_group) do
+    Investment.new(crypto: 'Dogecoin', amount: 55, user_id: user.id, group_id: 100_000)
+  end
+  let(:investment_to_ghost_user) do
+    Investment.new(crypto: 'Dogecoin', amount: 55, user_id: 100_000, group_id: group.id)
+  end
+
+  describe 'Validating when unexisted user or group is presented' do
+    it 'If group is not exist, it should not be valid' do
+      expect(investment_to_ghost_group).to_not be_valid
+    end
+
+    it 'If user is not exist, it should not be valid' do
+      expect(investment_to_ghost_user).to_not be_valid
+    end
+  end
 
   describe 'Investment can be created and validated' do
     it 'Investment can be created' do
@@ -32,6 +60,20 @@ RSpec.describe Investment, type: :model do
 
     it 'User id is missing' do
       expect(fatal_investment_user_id.valid?).to eq(false)
+    end
+  end
+
+  describe 'Amount in investment can be validated properly' do
+    it 'When there is a short investment is valid, and it is normal in cryptocurrencies investments' do
+      expect(short_investment).to be_valid
+    end
+
+    it 'When there is a too long investment is not valid, and it often is impossible' do
+      expect(long_investment).to_not be_valid
+    end
+
+    it 'When there is a negative investment, it is impossible' do
+      expect(negative_investment).to_not be_valid
     end
   end
 end
